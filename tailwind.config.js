e01a5405c18d52b3dc8e93a5e2ec3cde06629c83
saeedjamali89@gmail.com <mahdimem65@gmail.com>
2023-11-01 13:22:26 +0330

    Col-Span

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      container:{
+        center:true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '1rem',
+          lg: '2rem',
+          xl: '3rem',
+          '2xl': '6rem',
+        },
+  
+      }
+    },
   },
   plugins: [],
 }
