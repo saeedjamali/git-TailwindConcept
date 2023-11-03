24f079524d3957e7cbc9c9ca4c7c308f01c58025
saeedjamali89@gmail.com <saeedjamali89@gmail.com>
2023-11-03 10:23:10 +0330

    /* Components  are added in this file and  the classes are combined

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,19 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      container:{
-        center:true,
+      container: {
+        center: true,
         padding: {
-          DEFAULT: '1rem',
-          sm: '1rem',
-          lg: '2rem',
-          xl: '3rem',
-          '2xl': '6rem',
+          DEFAULT: "1rem",
+          sm: "1rem",
+          lg: "2rem",
+          xl: "3rem",
+          "2xl": "6rem",
         },
-  
-      }
+      },
     },
   },
   plugins: [],
-}
+};
 
+// Use Extend object for update tag
